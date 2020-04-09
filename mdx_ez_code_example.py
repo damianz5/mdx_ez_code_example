3b42bd52c4ab6330579497a9a91c80c43503adaa
Damian Zabawa <damian.zabawa@gmail.com>
2020-04-09 16:26:40 +0200

    Updated for python3 / Markdown 3

--- mdx_ez_code_example.py
+++ mdx_ez_code_example.py
@@ -12,38 +12,36 @@ License: [BSD](http://www.opensource.org/licenses/bsd-license.php)
 
 from __future__ import absolute_import
 from __future__ import unicode_literals
-import markdown
 from markdown import Extension
 from markdown.preprocessors import Preprocessor
-from markdown.extensions.codehilite import CodeHilite, CodeHiliteExtension
+from markdown.extensions.codehilite import CodeHilite, CodeHiliteExtension, parse_hl_lines
 import re
 
-# Global vars
-FENCED_BLOCK_RE = re.compile( \
-    r'(?P<fence>^(?:\[{2}code_example))[ ]*(\{?\.?(?P<lang>[a-zA-Z0-9_+-]*)\}?)?[ ]*\n(?P<code>.*?)(?<=\n)(?P<end>(?:code_example\]{2}))[ ]*$',
-    re.MULTILINE|re.DOTALL
-    )
-CODE_WRAP = '<pre><code%s>%s</code></pre>'
-LANG_TAG = ' class="%s"'
-RESULT = '''
-%s<div class="code_example"><div class="bd-example">%s</div>%s</div>%s
-'''
 
-class CodeExampleExtension(Extension):
+class EzCodeExampleExtension(Extension):
 
-    def extendMarkdown(self, md, md_globals):
-        """ Add CodeExampleBlockPreprocessor to the Markdown instance. """
+    def extendMarkdown(self, md):
+        """ Add EzCodeExampleBlockPreprocessor to the Markdown instance. """
         md.registerExtension(self)
 
-        md.preprocessors.add('ez_code_example',
-                                 CodeExampleBlockPreprocessor(md),
-                                 ">normalize_whitespace")
+        md.preprocessors.register(EzCodeExampleBlockPreprocessor(md), 'ez_code_example', 20)
 
 
-class CodeExampleBlockPreprocessor(Preprocessor):
+class EzCodeExampleBlockPreprocessor(Preprocessor):
+    FENCED_BLOCK_RE = re.compile(
+    r'''^(?P<fence>(?:\[{2}code_example))[ ]*
+(\{?\.?(?P<lang>[\w#.+-]*))?[ ]*
+(hl_lines=(?P<quot>\"|\')(?P<hl_lines>[0-9 ]*?)(?P=quot))?[ ]*
+(wrap=(?P<q2>\"|\')?(?P<wrap_tag>[\S\_\-]*?)(?P=q2)?)?[ ]*
+(class=(?P<q3>\"|\')?(?P<wrap_class>.*?)(?P=q3)?)?[ ]*
+\}?[ ]*
+\n(?P<code>.*?)(?<=\n)(?P<end>(?:code_example\]{2}))[ ]*$''', re.MULTILINE | re.DOTALL | re.VERBOSE
+    )
+    CODE_WRAP = '<pre><code%s>%s</code></pre>'
+    LANG_TAG = ' class="%s"'
 
     def __init__(self, md):
-        super(CodeExampleBlockPreprocessor, self).__init__(md)
+        super().__init__(md)
 
         self.checked_for_codehilite = False
         self.codehilite_conf = {}
@@ -53,7 +51,7 @@ class CodeExampleBlockPreprocessor(Preprocessor):
 
         # Check for code hilite extension
         if not self.checked_for_codehilite:
-            for ext in self.markdown.registeredExtensions:
+            for ext in self.md.registeredExtensions:
                 if isinstance(ext, CodeHiliteExtension):
                     self.codehilite_conf = ext.config
                     break
@@ -62,29 +60,48 @@ class CodeExampleBlockPreprocessor(Preprocessor):
 
         text = "\n".join(lines)
         while 1:
-            m = FENCED_BLOCK_RE.search(text)
+            m = self.FENCED_BLOCK_RE.search(text)
+
             if m:
                 lang = ''
+                wrap_tag = ''
+                wrap_class = ''
                 if m.group('lang'):
-                    lang = LANG_TAG % m.group('lang')
+                    lang = self.LANG_TAG % m.group('lang')
+
+                if m.group('wrap_tag'):
+                    wrap_tag = m.group('wrap_tag')
+
+                if m.group('wrap_class'):
+                    wrap_class = m.group('wrap_class')
 
                 # If config is not empty, then the codehighlite extension
-                # is enabled, so we call it to highlite the code
+                # is enabled, so we call it to highlight the code
                 if self.codehilite_conf:
-                    highliter = CodeHilite(m.group('code'),
-                            linenums=self.codehilite_conf['linenums'][0],
-                            guess_lang=self.codehilite_conf['guess_lang'][0],
-                            css_class=self.codehilite_conf['css_class'][0],
-                            style=self.codehilite_conf['pygments_style'][0],
-                            lang=(m.group('lang') or None),
-                            noclasses=self.codehilite_conf['noclasses'][0])
+                    highliter = CodeHilite(
+                        m.group('code'),
+                        linenums=self.codehilite_conf['linenums'][0],
+                        guess_lang=self.codehilite_conf['guess_lang'][0],
+                        css_class=self.codehilite_conf['css_class'][0],
+                        style=self.codehilite_conf['pygments_style'][0],
+                        use_pygments=self.codehilite_conf['use_pygments'][0],
+                        lang=(m.group('lang') or None),
+                        noclasses=self.codehilite_conf['noclasses'][0],
+                        hl_lines=parse_hl_lines(m.group('hl_lines'))
+                    )
 
                     code = highliter.hilite()
                 else:
-                    code = CODE_WRAP % (lang, self._escape(m.group('code')))
+                    code = self.CODE_WRAP % (lang, self._escape(m.group('code')))
 
-                placeholder = self.markdown.htmlStash.store(code, safe=True)
-                text = '%s\n<div class="ez-code-example">%s</div> \n %s\n%s' % (text[:m.start()], m.group('code'), placeholder, text[m.end():])
+                placeholder = self.md.htmlStash.store(code)
+
+                text = '{}\n<div class="ez-code-example">{}</div> \n {}\n{}'\
+                    .format(text[:m.start()], m.group('code'), placeholder, text[m.end():])
+
+                if wrap_tag and wrap_class:
+                    text = '<%s class="%s">\n%s\n</%s>' % (wrap_tag, wrap_class, text, wrap_tag)
+
             else:
                 break
         return text.split("\n")
@@ -98,10 +115,5 @@ class CodeExampleBlockPreprocessor(Preprocessor):
         return txt
 
 
-def makeExtension(configs=None):
-    return CodeExampleExtension()
-
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
+def makeExtension(**kwargs):  # pragma: no cover
+    return EzCodeExampleExtension(**kwargs)
